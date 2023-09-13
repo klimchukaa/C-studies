f6c787d4302ffd405fc1399d472fc3eca4ebdd8b
Aleksandr Klimchuk <Nanofulleren@yandex.ru>
2023-09-13 17:21:02 +0300

    Fixed solution

--- tasks/chocolate/chocolate.cpp
+++ tasks/chocolate/chocolate.cpp
@@ -1,7 +1,7 @@
 #include "chocolate.h"
 
 bool CanBreakSlices(int height, int width, int slices_amount) {
-    if (height * width < slices_amount) {
+    if (height * width <= slices_amount) {
         return false;
     }
     if (slices_amount % height == 0 || slices_amount % width == 0) {
