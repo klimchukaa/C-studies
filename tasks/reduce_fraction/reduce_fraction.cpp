fc8fa969196f59eabefb55dec334b4211aa1bba4
Aleksandr Klimchuk <Nanofulleren@yandex.ru>
2023-09-13 17:39:40 +0300

    First solution

--- tasks/reduce_fraction/reduce_fraction.cpp
+++ tasks/reduce_fraction/reduce_fraction.cpp
@@ -1,6 +1,24 @@
 #include "reduce_fraction.h"
 
+int64_t gcd(int64_t a, int64_t b) {
+    if (b == 0) {
+        return a;
+    }
+    return gcd(b, a % b);
+}
 
 int64_t ReduceFraction(int64_t numerator, int64_t denominator) {
-    return 1;
+    int64_t sign_of_numerator = 1;
+    if (numerator < 0) {
+        sign_of_numerator *= -1;
+        numerator *= -1;
+    }
+    if (denominator < 0) {
+        sign_of_numerator *= -1;
+        denominator *= -1;
+    }
+    int64_t d = gcd(numerator, denominator);
+    int64_t new_numer = numerator / d;
+    int64_t new_denom = denominator / d;
+    return new_denom + sign_of_numerator * new_numer;
 }
