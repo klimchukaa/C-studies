63411b0689663477cceed347723f5581c648129f
Aleksandr Klimchuk <Nanofulleren@yandex.ru>
2023-09-13 17:54:18 +0300

    First solution

--- tasks/palindrome/palindrome.cpp
+++ tasks/palindrome/palindrome.cpp
@@ -1,5 +1,20 @@
 #include "palindrome.h"
 
 bool IsPalindrome(const std::string& str) {
-    return false;
+    int64_t n = str.size();
+    int64_t left_it = 0;
+    int64_t right_it = n - 1;
+    while (left_it < right_it) {
+        if (str[left_it] == ' ') {
+            ++left_it;
+        } else if (str[right_it] == ' ') {
+            --right_it;
+        } else if (str[left_it] != str[right_it]) {
+            return false;
+        } else {
+            ++left_it;
+            --right_it;
+        }
+    }
+    return true;
 }
