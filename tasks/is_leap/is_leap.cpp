f96e717557b4f94887db3b74acc04fcc142603f7
Aleksandr Klimchuk <Nanofulleren@yandex.ru>
2023-09-16 09:56:55 +0300

    First solution

--- tasks/is_leap/is_leap.cpp
+++ tasks/is_leap/is_leap.cpp
@@ -1,5 +1,18 @@
 #include "is_leap.h"
 
+const int GOOD_YEARS = 400;
+const int BAD_YEARS = 100;
+
 bool IsLeap(int year) {
-    return true;
+    if (year % GOOD_YEARS == 0) {
+        return true;
+    }
+    if (year % BAD_YEARS == 0) {
+        return false;
+    }
+    if (year % 4 == 0) {
+        return true;
+    }
+    return false;
 }
+
