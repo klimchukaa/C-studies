763a62d909c3df3c9ffb4b5ca7c4a97116ebe7bb
Aleksandr Klimchuk <Nanofulleren@yandex.ru>
2023-09-18 22:08:18 +0300

    changed solution

--- tasks/fibonacci/fibonacci.cpp
+++ tasks/fibonacci/fibonacci.cpp
@@ -1,15 +1,15 @@
 #include "fibonacci.h"
 
 int64_t CalculateNthFibonacci(int64_t n) {
-    int64_t fibonacci[n + 1];
-    for (int64_t i = 0; i <= n; ++i) {
-        if (i == 0) {
-            fibonacci[i] = 0;
-        } else if (i == 1) {
-            fibonacci[i] = 1;
-        } else {
-            fibonacci[i] = fibonacci[i - 1] + fibonacci[i - 2];
-        }
+    if (n == 0) {
+        return 0;
     }
-    return fibonacci[n];
+    int64_t before_last = 0;
+    int64_t last = 1;
+    for (int64_t i = 1; i < n; ++i) {
+        int64_t new_last = last + before_last;
+        before_last = last;
+        last = new_last;
+    }
+    return last;
 }
