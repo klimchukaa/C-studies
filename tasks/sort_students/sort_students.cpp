895de1330313e7fcfada751faff0d9bed982e58a
Aleksandr Klimchuk <Nanofulleren@yandex.ru>
2023-09-30 17:49:20 +0300

    Added solution

--- tasks/sort_students/sort_students.cpp
+++ tasks/sort_students/sort_students.cpp
@@ -1,4 +1,19 @@
 #include "sort_students.h"
+#include <tuple>
+#include <algorithm>
+
+bool CmpByDate(const Student& a, const Student& b) {
+    return std::tie(a.birth_date, a.last_name, a.name) < std::tie(b.birth_date, b.last_name, b.name);
+}
+
+bool CmpByName(const Student& a, const Student& b) {
+    return std::tie(a.last_name, a.name, a.birth_date) < std::tie(b.last_name, b.name, b.birth_date);
+}
 
 void SortStudents(std::vector<Student>& students, SortKind sortKind) {
+    if (sortKind == SortKind::Date) {
+        std::sort(students.begin(), students.end(), CmpByDate);
+    } else {
+        std::sort(students.begin(), students.end(), CmpByName);
+    }
 }
