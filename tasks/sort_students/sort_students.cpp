865eec974f5a8909c6054e00ba99fe116448fcea
Aleksandr Klimchuk <Nanofulleren@yandex.ru>
2023-10-06 16:58:43 +0300

    Edited comparator

--- tasks/sort_students/sort_students.cpp
+++ tasks/sort_students/sort_students.cpp
@@ -3,11 +3,13 @@
 #include <algorithm>
 
 bool CmpByDate(const Student& a, const Student& b) {
-    return std::tie(a.birth_date, a.last_name, a.name) < std::tie(b.birth_date, b.last_name, b.name);
+    return std::tie(a.birth_date.year, a.birth_date.month, a.birth_date.day, a.last_name, a.name) < 
+             std::tie(b.birth_date.year, b.birth_date.month, b.birth_date.day, b.last_name, b.name);
 }
 
 bool CmpByName(const Student& a, const Student& b) {
-    return std::tie(a.last_name, a.name, a.birth_date) < std::tie(b.last_name, b.name, b.birth_date);
+    return std::tie(a.last_name, a.name, a.birth_date.year, a.birth_date.month, a.birth_date.day) < 
+             std::tie(b.last_name, b.name, b.birth_date.year, b.birth_date.month, b.birth_date.day);
 }
 
 void SortStudents(std::vector<Student>& students, SortKind sortKind) {
