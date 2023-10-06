78d725e03fba3f3cc6041fc93d90f26dbe78e8c0
Aleksandr Klimchuk <Nanofulleren@yandex.ru>
2023-10-06 17:53:34 +0300

    Fixed style

--- tasks/tests_checking/tests_checking.cpp
+++ tasks/tests_checking/tests_checking.cpp
@@ -3,7 +3,7 @@
 
 std::vector<std::string> StudentsOrder(const std::vector<StudentAction>& student_actions,
                                        const std::vector<size_t>& queries) {
-    std::vector <std::string> top, bottom;
+    std::vector<std::string> top, bottom;
     for (size_t i = 0; i < student_actions.size(); ++i) {
         if (student_actions[i].side == Side::Top) {
             top.push_back(student_actions[i].name);
@@ -11,14 +11,14 @@ std::vector<std::string> StudentsOrder(const std::vector<StudentAction>& student
             bottom.push_back(student_actions[i].name);
         }
     }
-    std::vector <std::string> order;
+    std::vector<std::string> order;
     for (size_t i = top.size() - 1; i >= 0; --i) {
         order.push_back(top[i]);
     }
     for (size_t i = 0; i < bottom.size(); ++i) {
         order.push_back(bottom[i]);
     }
-    std::vector <std::string> responses;
+    std::vector<std::string> responses;
     for (size_t i = 0; i < queries.size(); ++i) {
         responses.push_back(order[queries[i] - 1]);
     }
