170b5f7e2e7b73e06e8e010c1a92590245257db3
Aleksandr Klimchuk <Nanofulleren@yandex.ru>
2023-10-06 22:05:00 +0300

    Fixed solution

--- tasks/admission/admission.cpp
+++ tasks/admission/admission.cpp
@@ -8,6 +8,11 @@ bool CmpByScore(const Applicant& a, const Applicant& b) {
                                                b.student.birth_date.day, b.student.name);
 }
 
+bool CmpByName(const Student* a, const Student* b) {
+    return std::tie(a->name, a->birth_date.year, a->birth_date.month, a->birth_date.day) < 
+                std::tie(b->name, b->birth_date.year, b->birth_date.month, b->birth_date.day);
+}
+
 AdmissionTable FillUniversities(const std::vector<University>& universities, const std::vector<Applicant>& applicants) {
     std::vector<Applicant> applicants_sorted = applicants;
     std::sort(applicants_sorted.begin(), applicants_sorted.end(), CmpByScore);
@@ -16,15 +21,9 @@ AdmissionTable FillUniversities(const std::vector<University>& universities, con
     for (size_t i = 0; i < universities.size(); ++i) {
         available_places[universities[i].name] = universities[i].max_students;
     }
-    for (size_t i = 0; i < applicants_sorted.size(); ++i) {
-        for (size_t j = 0; j < applicants_sorted[i].wish_list.size(); ++j) {
-            if (available_places[applicants_sorted[i].wish_list[j]] > 0) {
-                Student* applicant = new Student(applicants_sorted[i].student);
-                admission_table[applicants_sorted[i].wish_list[j]].push_back(applicant);
-                --available_places[applicants_sorted[i].wish_list[j]];
-                break;
-            }
-        }
+    
+    for (size_t i = 0; i < universities.size(); ++i) {
+        std::sort(admission_table[universities[i].name].begin(), admission_table[universities[i].name].end(), CmpByName);
     }
     return admission_table;
 }
