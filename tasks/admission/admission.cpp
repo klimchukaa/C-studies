528c8cc97494c67dc750e652b494ce1d8b41d0d4
Aleksandr Klimchuk <Nanofulleren@yandex.ru>
2023-10-06 21:55:16 +0300

    Fixed solution

--- tasks/admission/admission.cpp
+++ tasks/admission/admission.cpp
@@ -2,29 +2,26 @@
 #include <tuple>
 #include <algorithm>
 
-bool operator<(const Student&a, const Student& b) {
-    return std::tie(a.birth_date.year, a.birth_date.month, a.birth_date.day, a.name) <
-                std::tie(b.birth_date.year, b.birth_date.month, b.birth_date.day, b.name);
-}
-
 bool CmpByScore(const Applicant& a, const Applicant& b) {
-    return std::tie(a.points, a.student) < std::tie(b.points, b.student);
+    return std::tie(b.points, a.student.birth_date.year, a.student.birth_date.month, a.student.birth_date.day, a.student.name) < 
+                std::tie(a.points, b.student.birth_date.year, b.student.birth_date.month, b.student.birth_date.day, b.student.name);
 }
 
 AdmissionTable FillUniversities(const std::vector<University>& universities, const std::vector<Applicant>& applicants) {
-    std::sort(applicants.begin(), applicants.end(), CmpByScore);
+    std::vector<Applicant> applicants_sorted = applicants;
+    std::sort(applicants_sorted.begin(), applicants_sorted.end(), CmpByScore);
     AdmissionTable admission_table;
     std::unordered_map<std::string, size_t> available_places;
     for (size_t i = 0; i < universities.size(); ++i) {
         admission_table.emplace(universities[i].name);
         available_places[universities[i].name] = universities[i].max_students;
     }
-    for (size_t i = 0; i < applicants.size(); ++i) {
-        for (size_t j = 0; j < applicants[i].wish_list.size(); ++j) {
-            if (available_places[applicants[i].wish_list[j]] > 0) {
-                Student* applicant = new Student(applicants[i].student);
-                admission_table[applicants[i].wish_list[j]].push_back(applicant);
-                --available_places[applicants[i].wish_list[j]];
+    for (size_t i = 0; i < applicants_sorted.size(); ++i) {
+        for (size_t j = 0; j < applicants_sorted[i].wish_list.size(); ++j) {
+            if (available_places[applicants_sorted[i].wish_list[j]] > 0) {
+                Student* applicant = new Student(applicants_sorted[i].student);
+                admission_table[applicants_sorted[i].wish_list[j]].push_back(applicant);
+                --available_places[applicants_sorted[i].wish_list[j]];
                 break;
             }
         }
