029e4e69b7f455b5ef38cac4b630d8c86c452ab1
Aleksandr Klimchuk <Nanofulleren@yandex.ru>
2023-10-07 20:25:58 +0300

    Fixed style

--- tasks/scorer/scorer.cpp
+++ tasks/scorer/scorer.cpp
@@ -31,7 +31,6 @@ ScoreTable GetScoredStudents(const Events& events, time_t score_time) {
             sorted_events[i]->task_name != sorted_events[i + 1]->task_name) {
             if (request_closed && task_accepted) {
                 score_table[sorted_events[i]->student_name].insert(sorted_events[i]->task_name);
-                
             }
             request_closed = true;
             task_accepted = false;
