b989139a8e03d376dabb31184ae9131f3c82c975
Aleksandr Klimchuk <Nanofulleren@yandex.ru>
2023-10-07 21:11:42 +0300

    Fixed solution

--- tasks/unixpath/unixpath.cpp
+++ tasks/unixpath/unixpath.cpp
@@ -6,6 +6,8 @@ std::string NormalizePath(std::string_view current_working_dir, std::string_view
         for (size_t i = 0; i < current_working_dir.size(); ++i) {
             cur.append(1, current_working_dir[i]);
         }
+    } else {
+        cur = "/";
     }
     for (size_t i = 0; i < path.size(); ++i) {
         if (path[i] == '.') {
@@ -14,8 +16,11 @@ std::string NormalizePath(std::string_view current_working_dir, std::string_view
                     cur.erase(cur.size() - 1);
                 }
             }
+            if (path.size() != 1) {
+                cur.erase(cur.size() - 1);
+            }
         } else if (path[i] != '/') {
-            if (i > 0 && path[i - 1] == '/') {
+            if (i > 0 && path[i - 1] == '/' && cur[cur.size() - 1] != '/') {
                 cur.append(1, '/');
             }
             cur.append(1, path[i]);
