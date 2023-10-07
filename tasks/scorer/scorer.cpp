ce4eac8f981a10bf41336c579b53ee1d0adfb5e9
Aleksandr Klimchuk <Nanofulleren@yandex.ru>
2023-10-07 19:00:38 +0300

    Fixed style

--- tasks/scorer/scorer.cpp
+++ tasks/scorer/scorer.cpp
@@ -3,8 +3,7 @@
 #include <algorithm>
 
 bool CmpByStudent(const Event* a, const Event* b) {
-    return std::tie(a->student_name, a->task_name, a->time) <
-           std::tie(b->student_name, b->task_name, b->time);
+    return std::tie(a->student_name, a->task_name, a->time) < std::tie(b->student_name, b->task_name, b->time);
 }
 
 ScoreTable GetScoredStudents(const Events& events, time_t score_time) {
@@ -28,8 +27,7 @@ ScoreTable GetScoredStudents(const Events& events, time_t score_time) {
         } else {
             request_closed = 0;
         }
-        if (i + 1 == sorted_events.size() || 
-            sorted_events[i]->student_name != sorted_events[i + 1]->student_name ||
+        if (i + 1 == sorted_events.size() || sorted_events[i]->student_name != sorted_events[i + 1]->student_name ||
             sorted_events[i]->task_name != sorted_events[i + 1]->task_name) {
             if (request_closed && task_accepted) {
                 score_table[sorted_events[i]->student_name].insert(sorted_events[i]->task_name);
