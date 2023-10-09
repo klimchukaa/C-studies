3781b546f0268ff00168b9bf5ee07ebbe425f31d
Aleksandr Klimchuk <Nanofulleren@yandex.ru>
2023-10-09 12:56:50 +0300

    Fixed solution

--- tasks/tests_checking/tests_checking.cpp
+++ tasks/tests_checking/tests_checking.cpp
@@ -1,29 +1,22 @@
 #include "tests_checking.h"
+
 #include <string_view>
 #include <algorithm>
+#include <deque>
 
 std::vector<std::string> StudentsOrder(const std::vector<StudentAction>& student_actions,
                                        const std::vector<size_t>& queries) {
-    std::vector<std::string> top;
-    std::vector<std::string> bottom;
+    std::deque<std::string_view> order_of_tests;
     for (size_t i = 0; i < student_actions.size(); ++i) {
         if (student_actions[i].side == Side::Top) {
-            top.push_back(student_actions[i].name);
+            order_of_tests.push_front(student_actions[i].name);
         } else {
-            bottom.push_back(student_actions[i].name);
+            order_of_tests.push_back(student_actions[i].name);
         }
-    }
-    std::vector<std::string> order;
-    std::reverse(top.begin(), top.end());
-    for (size_t i = 0; i < top.size(); ++i) {
-        order.push_back(top[i]);
-    }
-    for (size_t i = 0; i < bottom.size(); ++i) {
-        order.push_back(bottom[i]);
     }
     std::vector<std::string> responses;
     for (size_t i = 0; i < queries.size(); ++i) {
-        responses.push_back(order[queries[i] - 1]);
+        responses.emplace_back(order_of_tests[queries[i] - 1]);
     }
     return responses;
 }
