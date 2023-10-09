9e60850dc5aa5541ad133edeaac8262e4410e0cb
Aleksandr Klimchuk <Nanofulleren@yandex.ru>
2023-10-09 13:20:45 +0300

    Fixed solution

--- tasks/admission/admission.cpp
+++ tasks/admission/admission.cpp
@@ -1,4 +1,5 @@
 #include "admission.h"
+
 #include <tuple>
 #include <algorithm>
 
@@ -15,20 +16,20 @@ bool CmpByName(const Student* a, const Student* b) {
 
 AdmissionTable FillUniversities(const std::vector<University>& universities, const std::vector<Applicant>& applicants) {
     std::vector<const Applicant*> applicants_sorted;
-    for (size_t i = 0; i < applicants.size(); ++i) {
-        applicants_sorted.push_back(&applicants[i]);
+    for (const auto& applicant : applicants) {
+        applicants_sorted.push_back(&applicant);
     }
     std::sort(applicants_sorted.begin(), applicants_sorted.end(), CmpByScore);
     AdmissionTable admission_table;
     std::unordered_map<std::string, size_t> available_places;
-    for (size_t i = 0; i < universities.size(); ++i) {
-        available_places[universities[i].name] = universities[i].max_students;
+    for (const auto& [name, max_students] : universities) {
+        available_places[name] = max_students;
     }
     for (size_t i = 0; i < applicants_sorted.size(); ++i) {
-        for (size_t j = 0; j < applicants_sorted[i]->wish_list.size(); ++j) {
-            if (available_places[applicants_sorted[i]->wish_list[j]] > 0) {
-                admission_table[applicants_sorted[i]->wish_list[j]].push_back(&applicants_sorted[i]->student);
-                --available_places[applicants_sorted[i]->wish_list[j]];
+        for (const auto& university : applicants_sorted[i]->wish_list) {
+            if (available_places[university] > 0) {
+                admission_table[university].push_back(&applicants_sorted[i]->student);
+                --available_places[university];
                 break;
             }
         }
